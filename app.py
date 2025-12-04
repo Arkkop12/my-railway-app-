efb006fddf7e4ec9cf12be363cad5358ac80e500
Arkkop12 <163377911+Arkkop12@users.noreply.github.com>
2025-12-04 13:02:54 +0700

    Change response message in home route
    Updated home route response message.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Render PaaS!"
+    return "Hello from Railway! Aplikasi PaaS sederhana berhasil berjalan."
 
 if __name__ == "__main__":
     app.run()
